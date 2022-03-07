9706b4ee20ddd4cc0102ec2c63f544f8af1e9fed
Saidaman <68653877+Saidaman@users.noreply.github.com>
2022-03-07 13:23:43 -0500

    changes to main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <string>
+
 /*
  * Implementation of a cache simulator program in C++.
  * CSF Assignment 3
@@ -6,5 +9,15 @@
  */
 
 int main(int argc, char *argv[]) {
-
+    
+    //checking for command-line input
+    if (argc == 7) {
+        int numSets = std::stoi(argv[1]); //turns type char into an int
+        int numBlocks = std::stoi(argv[2]);
+        int numBytes = std::stoi(argv[3]);
+        std::string writeAllocation = argv[4];
+        std::string howToWrite = argv[5];
+        
+    }
+    
 }
