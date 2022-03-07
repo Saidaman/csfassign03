4cf13c48fb1dc05ea785acdafe6c0dbfe5df3fd6
Saidaman <68653877+Saidaman@users.noreply.github.com>
2022-03-07 13:32:13 -0500

    changes to main

--- main.cpp
+++ main.cpp
@@ -11,13 +11,23 @@
 int main(int argc, char *argv[]) {
     
     //checking for command-line input
-    if (argc == 7) {
+    if (argc == 7) { //do we even need to check for this?
         int numSets = std::stoi(argv[1]); //turns type char into an int
         int numBlocks = std::stoi(argv[2]);
         int numBytes = std::stoi(argv[3]);
         std::string writeAllocation = argv[4];
         std::string howToWrite = argv[5];
-        
+        std::string eviction = argv[6];
+    }
+    else { //if there are not 6 other parameters passed in from command line other than ./csim
+        std::cerr << "Not enough parameters passed in through command line\n";
+        return 1;
+    }
+
+    //command line input error-checking
+    if ((numBlocks % 2) != 0) {
+        std::cerr << "block size is not a power of 2";
+        return 2;
     }
     
 }
