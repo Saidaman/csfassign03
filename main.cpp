b0c5e9742d95b98a00915e1e7d203c171658a4ae
SyedHossain20 <syed.shayan01@gmail.com>
2022-03-16 13:01:26 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
 
 /*
  * Implementation of a cache simulator program in C++.
@@ -14,14 +16,26 @@ void printOutput(int counts[]);
 int logBase2(int num);
 int isPowerOfTwo(int num);
 
-class Slot {
-    unsigned tag;
-    unsigned index;
-    unsigned offset;
-    bool valid;
-    bool dirty;
-    unsigned load_ts; //timestamp for FIFO
-    unsigned access_ts; //timestamp for LRU
+class Block {
+    public: //get/set
+        unsigned offset;
+        unsigned index;
+        unsigned tag;
+        bool valid;
+        bool dirty;
+        unsigned load_ts; //timestamp for FIFO
+        unsigned access_ts; //timestamp for LRU
+
+    public:
+        Block(unsigned offset, unsigned index, unsigned tag, bool valid, bool dirty, unsigned load_ts, unsigned access_ts) {
+            this->offset = offset;
+            this->index = index;
+            this->tag = tag;
+            this->valid = valid;
+            this->dirty = dirty;
+            this->load_ts = load_ts;
+            this->access_ts = access_ts;
+        }
 };
 
     int main(int argc, char *argv[]) {
@@ -36,12 +50,9 @@ class Slot {
     int counts[7] = {0}; // counts[0] -> total loads, counts[1] -> total stores, counts[2] -> load hits
     // counts[3] -> load misses, counts[4] -> store hits, counts[5] -> store misses, counts[6] -> total cycles
 
-    map cashe[numSets];
-    for (i= 0, i < cashe.size(); i++) {
-        cashe[i] = new vector[numBlocks];
-    }
-
-
+    // initialize the cache: key = index, vector of blocks = sets
+    std::map <unsigned, std::vector<Block>> cache;
+    
     //checking for command-line input
     if (argc == 7)
     {
@@ -70,10 +81,26 @@ class Slot {
         int garbageVal;
         std::cin >> garbageVal;
 
-        //still need to do caching stuff
-        int offsetNumBits = logBase2(numBytes);
-        int indexNumBits = logBase2(numSets);
-        int tagNumBits = 32 - offsetNumBits - indexNumBits;
+        int numOffsetBits = logBase2(numBytes);
+        int numIndexBits = logBase2(numSets);
+
+        //logBase2(numBytes) gives you the number of offset bits. Then,
+        //we left shift 1 so it can have logBase2(numBytes) number of trailing zeros.
+        //Then, we subtract 1 so that we can have a binary number with
+        //logBase2(numBytes) number of bits of just 1s. We later use this result
+        //with the & operator to isolate those bits in the address.
+        unsigned offsetBits = (1 << numOffsetBits) - 1;
+        //similar approach as above   
+        unsigned indexBits = (1 << numIndexBits) - 1;
+        //similar approach as above
+        unsigned tagBits = (1 << (32 - numOffsetBits - numIndexBits)) - 1;
+        
+        unsigned offset = address & offsetBits;
+        address >>= numOffsetBits;
+        unsigned index = address & indexBits;
+        unsigned tag = address >> numIndexBits;
+
+        Block currBlock(offset, index, tag, false, false, 0, 0);
 
         //couunts loads and stores
         if (performField.compare("l") == 0) {
